aceb194d6308acc40ae89e86b9cbdebb52773396
alfihopkins <90531062+alfihopkins@users.noreply.github.com>
2023-07-25 03:39:45 -0700

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0xe5b7fff39e86b4234f4a5e56f3099aa3795f8c8a";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
